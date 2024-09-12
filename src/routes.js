4139f0c6e1f5147d8db9a71649a2df2e1776f6c3
AniBukhrashvili <92999532+AniBukhrashvili@users.noreply.github.com>
2024-09-12 13:49:07 +0400

    cleanup && add routes

---
+++ src/routes.js
@@ -0,0 +1,20 @@
+import { createBrowserRouter } from "react-router-dom";
+
+import Home from "./pages";
+import RealEstate from "./pages/real-estate";
+import NotFound from "./pages/404";
+
+export const routes = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/real-estate",
+    element: <RealEstate />,
+  },
+  {
+    path: "*",
+    element: <NotFound />,
+  },
+]);
